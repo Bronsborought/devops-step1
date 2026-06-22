253b51ca6781463fbdceb77f28282e97132d8907
DmytroT <tarakanovds@gmail.com>
2026-06-23 02:05:18 +0300

    Fix Docker build context with .dockerignore and update Terraform config

--- main.tf
+++ main.tf
@@ -9,11 +9,16 @@ terraform {
 
 provider "docker" {}
 
+# Instruct Terraform to build the image from the local Dockerfile
 resource "docker_image" "web_page" {
   name = "my-first-web-page:latest"
+  build {
+    context = "."
+  }
 }
 
 resource "docker_container" "nginx_server" {
+  # Use the unique ID of the newly built image
   image = docker_image.web_page.image_id
   name  = "terraform-managed-nginx"
   
